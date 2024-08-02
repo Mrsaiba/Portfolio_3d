0e5cb5e2c270ae3268bd60d6c64588dfe4bddbc9
Mrsaiba <jitsaiba@gmail.com>
2024-08-02 22:22:40 +0530

    added navbar links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink to="/">
+        <p>JS</p>
+      </NavLink>
+    </header>
+  );
+};
+
+export default Navbar;
