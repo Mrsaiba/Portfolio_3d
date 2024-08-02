5af5d2611e04dce1fe40cf41a875e4b7d8703e77
Mrsaiba <jitsaiba@gmail.com>
2024-08-02 22:17:57 +0530

    added routing for nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import React from "react";
 
 const App = () => {
-  return <main></main>;
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/project" element={"Projects"} />
+          <Route path="/contact" element={"Contact"} />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
